c376c92845c4677d72d44e63ea2b1229fbcf6c42
cmichaelsd <cmichaelsd@gmail.com>
2018-10-24 00:06:39 -0700

    Test 3

--- src/rest-server/controllers/monthlyDonation.js
+++ src/rest-server/controllers/monthlyDonation.js
@@ -7,13 +7,10 @@ const monthlyDonation = {
     const input = JSON.parse(req.body);
     console.log(typeof input, input);
     try {
-      // Define a Product
       const product = await stripe.products.create({
         name: "Give Monthly",
         type: "service"
       });
-      console.log("PRODUCT", product);
-      // Define Products Plan
       const varPlan = await stripe.plans.create({
         product: product.id,
         nickname: "Give Monthly USD",
@@ -21,27 +18,22 @@ const monthlyDonation = {
         interval: "month",
         amount: input.amount
       });
-      console.log("PLAN", varPlan);
-      // Create Customer
       const customer = await stripe.customers.create({
         email: input.email,
         source: input.token
       });
-      console.log("CUSTOMER", customer);
-      // Store Customer Email and ID in DB
       const subscription = await stripe.subscriptions.create({
         customer: customer.id,
         items: [{ plan: varPlan.id }]
       });
-      console.log("SUBSCRIPTION", subscription);
 
       // Confirm data
       console.log(
         "REQUIRED DATA START",
-        input.name,
-        input.lastname,
+        "INPUT OBJECT",
+        input,
+        "CUSTOMER ID",
         customer.id,
-        input.email,
         "REQUIRED DATA END"
       );
       // Add required data to PSQL
