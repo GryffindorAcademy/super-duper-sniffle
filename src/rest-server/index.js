6b5a4241fccd812f005d2c598e3a26da8e625e75
cmichaelsd <cmichaelsd@gmail.com>
2019-01-24 15:38:59 -0800

    Missed a line turning off chalk.

--- src/rest-server/index.js
+++ src/rest-server/index.js
@@ -1,14 +1,14 @@
 const http = require("http");
 const app = require("./config/express");
 const PORT = process.env.PORT || 6060;
-const { success, error } = require("./lib/log");
+// const { success, error } = require("./lib/log");
 
 const server = http.createServer(app);
 
 const init = () => {
   server.listen(PORT, err => {
     if (err) {
-      error("Server crashed for the following reasons: ");
+      console.log("Server crashed for the following reasons: ");
       throw err;
     }
     console.log(`Successfully connected to port ${PORT}`);
